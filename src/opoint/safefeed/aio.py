37ee899ff7c5868cebb136174eb1fa154198bd80
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2025-01-17 14:46:10 +0100

    Handle expected_rate and set interval and num_art automatically

--- src/opoint/safefeed/aio.py
+++ src/opoint/safefeed/aio.py
@@ -15,41 +15,45 @@ class SafefeedOptions(TypedDict, total=False):
     interval: int
     timeout: int
     base_url: str
-    batch_size: int
+    num_art: int
 
 
 class SafefeedClient:
     """Asynchronous Safefeed client using aiohttp"""
 
     key: str
-    doc_format: Literal["json"] | Literal["xml"]
+    doc_format: Literal["json"]
     interval: int
     timeout: int
     base_url: str
-    batch_size: int
+    num_art: int
     lastid: int | None
+    expected_rate: float | None
     _session: aiohttp.ClientSession
     _last_num: int
     _last_request_time: datetime.datetime | None = None
+    _autoconfig: bool = True
 
     def __init__(
         self,
         key: str,
-        doc_format: Literal["json"] | Literal["xml"] = "json",
-        interval: int = 10,
-        timeout: int = 5,
+        interval: int | None = None,
+        timeout: int = 10,
         lastid: int | None = None,
         base_url: str = "https://feed.opoint.com/safefeed.php",
-        batch_size: int = 500,
+        num_art: int | None = None,
+        expected_rate: float | None = None,
     ) -> None:
         self.key = key
-        self.interval = interval
-        self.doc_format = doc_format
+        self.interval = interval if interval is not None else 60
         self.timeout = timeout
         self.base_url = base_url
-        self.batch_size = batch_size
+        self.num_art = num_art if num_art is not None else 500
         self.lastid = lastid
+        self.expected_rate = expected_rate
         self._session = aiohttp.ClientSession()
+        if interval or num_art:
+            self._autoconfig = False
 
     async def __aenter__(self) -> Self:
         return self
@@ -57,12 +61,21 @@ class SafefeedClient:
     async def __aexit__(self, *exc: Any) -> None:
         return await self._session.close()
 
+    @property
+    def is_behind(self) -> bool:
+        """Denotes whether the client believes that it is "behind" and currently catching up the the current, based on recent responses."""
+        return self._last_num > (self.num_art) * 0.9
+
     async def _fetch_articles(
         self, lastid: int | None = None, size: int | None = None
     ) -> _BaseRequestContextManager[aiohttp.ClientResponse]:
         now = datetime.datetime.now()
-        target = self._last_request_time + datetime.timedelta(seconds=self.interval) if self._last_request_time else now
-        if now < target and self._last_num < 0.9 * self.batch_size:
+        target = (
+            self._last_request_time + datetime.timedelta(seconds=self.interval)
+            if self._last_request_time
+            else now
+        )
+        if now < target and not self.is_behind:
             await asyncio.sleep((target - now).total_seconds())
 
         self._last_request_time = datetime.datetime.now()
@@ -71,10 +84,11 @@ class SafefeedClient:
             self.base_url,
             params={
                 "key": self.key,
-                "doc_format": self.doc_format,
-                "lastid": i if (i := lastid or self.lastid) is not None else '?',
-                "num_art": size or self.batch_size,
+                "doc_format": "json",
+                "lastid": i if (i := lastid or self.lastid) is not None else "?",
+                "num_art": size or self.num_art,
             },
+            timeout=aiohttp.ClientTimeout(total=self.timeout),
         )
 
     async def get_articles(
@@ -101,16 +115,23 @@ class SafefeedClient:
             try:
                 self.lastid = data["searchresult"]["search_start"]
                 self._last_num = data["searchresult"].get("documents", 0)
+                self.expected_rate = self.expected_rate or data["searchresult"].get(
+                    "expected_rate"
+                )
+                if self.expected_rate and self._autoconfig:
+                    logging.debug("Configuring parameters")
+                    self.interval = 70 / self.expected_rate**0.5
+                    self.num_art = 100 * self.expected_rate**0.5
+
+                logging.debug(
+                    f"expected_rate: {self.expected_rate}, interval: {self.interval}, num_art: {self.num_art}"
+                )
             except KeyError:
                 logging.warn(
                     "Could not update internal state. JSON response is probably malformed somehow."
                 )
 
             return data
-
-    async def get_articles_xml(self) -> str:
-        async with await self._fetch_articles() as response:
-            return await response.text()
 
     # async def seek(self, timestamp: int) -> int:
     #     return 0
