712621e465ba3b88c32f7c58f5a8b25f6f82e848
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2025-01-22 17:08:26 +0100

    Improve autoconfig and add configurable log levels

--- src/opoint/safefeed/aio.py
+++ src/opoint/safefeed/aio.py
@@ -7,6 +7,9 @@ from typing import Any, Literal, Self, TypedDict, cast
 import aiohttp
 
 from opoint.safefeed.api import FeedResponse
+from opoint.safefeed.common import FeedConfiguration
+
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 class SafefeedOptions(TypedDict, total=False):
@@ -37,12 +40,10 @@ class SafefeedClient:
 
     key: str
     doc_format: Literal["json"]
-    interval: int
+    config: FeedConfiguration
     timeout: int
     base_url: str
-    num_art: int
     lastid: int | None
-    expected_rate: float | None
     _session: aiohttp.ClientSession
     _last_num: int
     _last_request_time: datetime.datetime | None = None
@@ -57,17 +58,18 @@ class SafefeedClient:
         base_url: str = "https://feed.opoint.com/safefeed.php",
         num_art: int | None = None,
         expected_rate: float | None = None,
+        log_level: int = logging.ERROR
     ) -> None:
+        global logger
         self.key = key
-        self.interval = interval if interval is not None else 60
         self.timeout = timeout
         self.base_url = base_url
-        self.num_art = num_art if num_art is not None else 500
         self.lastid = lastid
-        self.expected_rate = expected_rate
         self._session = aiohttp.ClientSession()
-        if interval or num_art or expected_rate:
-            self._autoconfig = False
+        self.config = FeedConfiguration(
+            interval=interval, num_art=num_art, expected_rate=expected_rate
+        )
+        logger.setLevel(log_level)
 
     async def __aenter__(self) -> Self:
         return self
@@ -78,7 +80,7 @@ class SafefeedClient:
     @property
     def is_behind(self) -> bool:
         """Denotes whether the client believes that it is "behind" and currently catching up the the current, based on recent responses."""
-        return self._last_num > (self.num_art) * 0.95
+        return self._last_num > (self.config.num_art.value) * 0.95
 
     async def get_articles(
         self, lastid: int | None = None, size: int | None = None
@@ -86,16 +88,16 @@ class SafefeedClient:
         """Get the next batch of articles"""
         now = datetime.datetime.now()
         target = (
-            self._last_request_time + datetime.timedelta(seconds=self.interval)
+            self._last_request_time + datetime.timedelta(seconds=self.config.interval.value)
             if self._last_request_time
             else now
         )
         if now < target and not self.is_behind:
-            logging.info(
+            logger.info(
                 f"Sleeping {(target - now).total_seconds()} seconds to respect interval setting"
             )
             await asyncio.sleep((target - now).total_seconds())
-            logging.info("Proceeding")
+            logger.info("Proceeding")
 
         self._last_request_time = datetime.datetime.now()
 
@@ -105,7 +107,7 @@ class SafefeedClient:
                 "key": self.key,
                 "doc_format": "json",
                 "lastid": i if (i := lastid or self.lastid) is not None else "?",
-                "num_art": size or self.num_art,
+                "num_art": size or self.config.num_art.value,
             },
             timeout=aiohttp.ClientTimeout(total=self.timeout),
         ) as response:
@@ -114,45 +116,34 @@ class SafefeedClient:
                 # TODO: Check HTTP status codes and stuff
                 data = cast(FeedResponse, await response.json())
             except aiohttp.ContentTypeError:
-                logging.error("Content-Type is not 'application/json'")
+                logger.error("Content-Type is not 'application/json'")
                 return None
             except UnicodeDecodeError:
-                logging.error("Could not decode Unicode data. This is very bad!")
+                logger.error("Could not decode Unicode data. This is very bad!")
                 return None
             except json.JSONDecodeError as e:
-                logging.error("Could not decode JSON response body.", e)
-                logging.debug("Full response body follows:")
-                logging.debug(await response.text())
+                logger.error("Could not decode JSON response body.", e)
+                logger.debug("Full response body follows:")
+                logger.debug(await response.text())
                 return None
 
             try:
                 self.lastid = data["searchresult"]["search_start"]
                 self._last_num = data["searchresult"].get("documents", 0)
-                logging.debug(f"Got {data['searchresult']['documents']} articles")
-                if self._autoconfig and not self.is_behind:
-                    logging.debug("Configuring parameters")
-                    self.expected_rate = 0.9 * (self.expected_rate or 40) + 0.1 * (
-                        data["searchresult"].get("expected_rate")
-                        or (
-                            data["searchresult"]["documents"]
-                            and (data["searchresult"]["documents"] / self.interval)
-                            or self.expected_rate
-                            or 40
-                        )
-                    )
-                    self.interval = min(60 / self.expected_rate**0.5, 900)
-                    self.num_art = max(120 * self.expected_rate**0.5, 50.0)
+                logger.debug(f"Got {data['searchresult']['documents']} articles")
+                logger.debug("Configuring parameters")
+                self.config.update(self._last_num)
 
                 if not self.is_behind:
-                    logging.debug(
-                        f"expected_rate: {(self.expected_rate or 0)}/s, interval: {self.interval}, num_art: {self.num_art}"
+                    logger.debug(
+                        f"expected_rate: {(self.config.expected_rate.value)}/s, interval: {self.config.interval.value}, num_art: {self.config.num_art.value}"
                     )
                 else:
-                    logging.debug(
+                    logger.debug(
                         "Is behind, cannot estimate rate or configure interval/num_art"
                     )
             except KeyError:
-                logging.warn(
+                logger.warn(
                     "Could not update internal state. JSON response is probably malformed somehow."
                 )
 
--- src/opoint/safefeed/sync.py
+++ src/opoint/safefeed/sync.py
@@ -2,11 +2,14 @@ import datetime
 import json
 import logging
 import time
-from typing import Any, Literal, Self, TypedDict, cast
+from typing import Literal, Self, TypedDict, cast
 
 import requests
 
 from opoint.safefeed.api import FeedResponse
+from opoint.safefeed.common import FeedConfiguration
+
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 class SafefeedOptions(TypedDict, total=False):
@@ -37,12 +40,10 @@ class SyncSafefeedClient:
 
     key: str
     doc_format: Literal["json"]
-    interval: int
+    config: FeedConfiguration
     timeout: int
     base_url: str
-    num_art: int
     lastid: int | None
-    expected_rate: float | None
     _last_num: int
     _last_request_time: datetime.datetime | None = None
     _autoconfig: bool = True
@@ -56,21 +57,22 @@ class SyncSafefeedClient:
         base_url: str = "https://feed.opoint.com/safefeed.php",
         num_art: int | None = None,
         expected_rate: float | None = None,
+        log_level: int = logging.ERROR
     ) -> None:
+        global logger
         self.key = key
-        self.interval = interval if interval is not None else 60
         self.timeout = timeout
         self.base_url = base_url
-        self.num_art = num_art if num_art is not None else 500
         self.lastid = lastid
-        self.expected_rate = expected_rate
-        if interval or num_art or expected_rate:
-            self._autoconfig = False
+        self.config = FeedConfiguration(
+            interval=interval, num_art=num_art, expected_rate=expected_rate
+        )
+        logger.setLevel(log_level)
 
     @property
     def is_behind(self) -> bool:
         """Denotes whether the client believes that it is "behind" and currently catching up the the current, based on recent responses."""
-        return self._last_num > (self.num_art) * 0.95
+        return self._last_num > (self.config.num_art.value) * 0.95
 
     def get_articles(
         self, lastid: int | None = None, size: int | None = None
@@ -78,16 +80,17 @@ class SyncSafefeedClient:
         """Get the next batch of articles"""
         now = datetime.datetime.now()
         target = (
-            self._last_request_time + datetime.timedelta(seconds=self.interval)
+            self._last_request_time
+            + datetime.timedelta(seconds=self.config.interval.value)
             if self._last_request_time
             else now
         )
         if now < target and not self.is_behind:
-            logging.info(
+            logger.info(
                 f"Sleeping {(target - now).total_seconds()} seconds to respect interval setting"
             )
             time.sleep((target - now).total_seconds())
-            logging.info("Proceeding")
+            logger.info("Proceeding")
 
         self._last_request_time = datetime.datetime.now()
 
@@ -97,7 +100,7 @@ class SyncSafefeedClient:
                 "key": self.key,
                 "doc_format": "json",
                 "lastid": i if (i := lastid or self.lastid) is not None else "?",
-                "num_art": size or self.num_art,
+                "num_art": size or self.config.num_art.value,
             },
             timeout=self.timeout,
         )
@@ -106,42 +109,32 @@ class SyncSafefeedClient:
             # TODO: Check HTTP status codes and stuff
             data = cast(FeedResponse, response.json())
         except UnicodeDecodeError:
-            logging.error("Could not decode Unicode data. This is very bad!")
+            logger.error("Could not decode Unicode data. This is very bad!")
             return None
         except json.JSONDecodeError as e:
-            logging.error("Could not decode JSON response body.", e)
-            logging.debug("Full response body follows:")
-            logging.debug(response.text)
+            logger.error("Could not decode JSON response body.", e)
+            logger.debug("Full response body follows:")
+            logger.debug(response.text)
             return None
 
         try:
             self.lastid = data["searchresult"]["search_start"]
             self._last_num = data["searchresult"].get("documents", 0)
-            logging.debug(f"Got {data['searchresult']['documents']} articles")
-            if self._autoconfig and not self.is_behind:
-                logging.debug("Configuring parameters")
-                self.expected_rate = 0.9 * (self.expected_rate or 40) + 0.1 * (
-                    data["searchresult"].get("expected_rate")
-                    or (
-                        data["searchresult"]["documents"]
-                        and (data["searchresult"]["documents"] / self.interval)
-                        or self.expected_rate
-                        or 40
-                    )
-                )
-                self.interval = min(60 / self.expected_rate**0.5, 900)
-                self.num_art = max(120 * self.expected_rate**0.5, 50.0)
+            logger.debug(f"Got {data['searchresult']['documents']} articles")
+            if not self.is_behind:
+                logger.debug("Configuring parameters")
+                self.config.update(self._last_num)
 
             if not self.is_behind:
-                logging.debug(
-                    f"expected_rate: {(self.expected_rate or 0)}/s, interval: {self.interval}, num_art: {self.num_art}"
+                logger.debug(
+                    f"expected_rate: {(self.config.expected_rate.value)}/s, interval: {self.config.interval.value}, num_art: {self.config.num_art.value}"
                 )
             else:
-                logging.debug(
+                logger.debug(
                     "Is behind, cannot estimate rate or configure interval/num_art"
                 )
         except KeyError:
-            logging.warn(
+            logger.warn(
                 "Could not update internal state. JSON response is probably malformed somehow."
             )
 
@@ -158,8 +151,8 @@ class SyncSafefeedClient:
 
 
 def main() -> None:
-    with SyncSafefeedClient("sample-token") as client:
-        print(client.get_articles())
+    client = SyncSafefeedClient("sample-token")
+    print(client.get_articles())
 
 
 if __name__ == "__main__":
